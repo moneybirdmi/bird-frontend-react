23a1066de05440eff9bc354ea76b0e7fa5acee03
moneybirdmi <money.bird.mi@gmail.com>
2021-02-24 16:10:20 +0500

    changed abi, contract address

--- src/contracts/addresses.js
+++ src/contracts/addresses.js
@@ -1,9 +1,9 @@
 // This address points to a dummy ERC20 contract deployed on Ethereum Mainnet,
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
-  ceaErc20: "0x877FE2925774219d8C6649D12d339d8F04c72d0f",
-  birdErc20: "0x877FE2925774219d8C6649D12d339d8F04c72d0f",
-  kovan: "0x873C83291691eE1e8c840dFB463c2c75406f2b1d"
+  ceaErc20: '0x877FE2925774219d8C6649D12d339d8F04c72d0f',
+  birdErc20: '0xd5b68817a015806d26532c2cc07c22420b99cfc4',
+  kovan: '0xc7953af803b8d64c909080c4e143875470c394d8',
 };
 
 export default addresses;
